83c5e95693812736335be41020115fde75063c5f
Abdullah Bilir <abdullahblrr@gmail.com>
2024-03-17 20:41:53 +0300

    useeffect filtreleme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+import UlkeListe from "./UlkeListe";
+
 function App() {
-  return App;
+  return (
+    <>
+      <div className="container my-3">
+        <UlkeListe />
+      </div>
+    </>
+  );
 }
 
 export default App;
